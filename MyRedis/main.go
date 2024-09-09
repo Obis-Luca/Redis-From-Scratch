12a08abeba0a8219613162e05663a3a22888805a
luca <luca.obis@gmail.com>
2024-09-10 00:49:17 +0300

    client + readRESP

--- MyRedis/main.go
+++ MyRedis/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	l, err := net.Listen("tcp", ":6379")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer conn.Close()
+
+	for {
+		buf := make([]byte, 1024)
+
+		_, err = conn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("error reading from client: ", err.Error())
+			os.Exit(1)
+		}
+
+		conn.Write([]byte("+Ok\r\n"))
+	}
 }
